f733a11b7e29957dfbf5b8a268d74d14672ea976
dennnoval <dennnoval@gmail.com>
2022-06-29 10:24:10 +0700

    Create Users.java

---
+++ app/src/main/java/javakominfo/backend/entity/Users.java
@@ -0,0 +1,52 @@
+package javakominfo.backend.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Users implements Serializable {
+
+	private String username;
+	private String password;
+	private String email;
+	private Role role;
+	private Date createdAt;
+  
+  public Users() {}
+
+	public Users(String u, String p, String e, String r) {
+		username = u;
+		password = p;
+		email = e;
+		role = r.equals("SUPERADMIN") ? Role.SUPERADMIN : r.equals("ADMIN") ? Role.ADMIN : Role.GUEST;
+		createdAt = new Date(System.currentTimeMillis());
+	}
+
+	public Users(String u, String p, String e, String r, Date ca) {
+		username = u;
+		password = p;
+		email = e;
+		role = r.equals("SUPERADMIN") ? Role.SUPERADMIN : r.equals("ADMIN") ? Role.ADMIN : Role.GUEST;
+		createdAt = ca;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+}
